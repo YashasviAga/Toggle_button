927d66862924196aa1f93019c94ffa54a82d4f27
YashasviAga <140539073+YashasviAga@users.noreply.github.com>
2023-08-10 22:17:29 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 function toggle_click() {
     let backdrop = document.getElementById('body');
-    backdrop.classList.add('dark');
-    let heading = document.getElementById('title');
-    heading.classList.remove('theme_light');
-    heading.classList.add('theme_dark');
-    let togglebutton = document.getElementById('toggle_button');
-    togglebutton.classList.remove('toggle_light');
-    togglebutton.classList.add('toggle_dark');
-    let slidertog = document.getElementById('slider');
-    slidertog.classList.remove('left_light');
-    slidertog.classList.add('right_dark');
+    if (backdrop.classList.contains('light')) {
+        backdrop.classList.remove('light');
+        backdrop.classList.add('dark');
+        let heading = document.getElementById('title');
+        heading.classList.remove('theme_light');
+        heading.classList.add('theme_dark');
+        let togglebutton = document.getElementById('toggle_button');
+        togglebutton.classList.remove('toggle_light');
+        togglebutton.classList.add('toggle_dark');
+        let slidertog = document.getElementById('slider');
+        slidertog.classList.remove('left_light');
+        slidertog.classList.add('right_dark');
+    }
+    else{
+        backdrop.classList.remove('dark');
+        backdrop.classList.add('light');
+        let heading = document.getElementById('title');
+        heading.classList.remove('theme_dark');
+        heading.classList.add('theme_light');
+        let togglebutton = document.getElementById('toggle_button');
+        togglebutton.classList.remove('toggle_dark');
+        togglebutton.classList.add('toggle_light');
+        let slidertog = document.getElementById('slider');
+        slidertog.classList.remove('right_dark');
+        slidertog.classList.add('left_light');
+    }
+
 }
